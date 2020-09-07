9f1f024dfc94e5afa48efe496d91a2d0a35f5516
Valentin Perrez <valentin.perrez@bluewin.ch>
2020-09-07 21:13:30 +0200

    Erstelle Plugin.

---
+++ Plugin/src/main/java/mcplugin/mcplugin/Mcplugin.java
@@ -0,0 +1,17 @@
+package mcplugin.mcplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mcplugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
