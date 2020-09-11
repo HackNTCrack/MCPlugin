f5261de7a9730897de69576fe3fdf1887883a44c
Valentin Perrez <valentin.perrez@bluewin.ch>
2020-09-11 18:36:09 +0200

    Füge /gm hinzu.

--- Plugin/src/main/java/mcplugin/mcplugin/Mcplugin.java
+++ Plugin/src/main/java/mcplugin/mcplugin/Mcplugin.java
@@ -1,5 +1,7 @@
 package mcplugin.mcplugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Mcplugin extends JavaPlugin {
@@ -13,5 +15,15 @@ public final class Mcplugin extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+
+        if (cmd.getName().equalsIgnoreCase("gm")) {
+
+            return true;
+
+        }
+        return false;
     }
 }
